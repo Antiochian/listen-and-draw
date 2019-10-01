8c5eee70f4312f9305ac4f90b9863731ac898c71
Antioch <53316182+Antiochian@users.noreply.github.com>
2019-10-01 04:57:56 +0100

    Added current prototype

--- listen-and-draw.py
+++ listen-and-draw.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Mon Sep 30 22:57:00 2019
+
+@author: user
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sat Sep 28 00:41:22 2019
 MSPaint clone
 @author: user
@@ -9,100 +16,147 @@ import pygame
 import sys
 import math
 from mutagen.mp3 import MP3
-# CONFIGUREABLE PARAMETERS #
-#filename = "audio/shorttest.mp3"
-filename = "audio/longtest.mp3"
-(Nx,Ny) = (600,400) #windowsize
-scrollheight = 700 #total height of canvas
-FPS = 30
-thickness = 1 #starting thickness
+import random
 
+# CONFIGUREABLE PARAMETERS #
+filename = "audio/arttest.mp3"
+#filename = "audio/bloodhail.mp3"
+loopnumber = 1
+(Nx,Ny) = (700,400) #windowsize
+scrollwidth = 1366 #total height of canvas
+FPS = 60
+thickness = 3 #starting thickness
+trackbarposition = "under"
+trackbarposition = "above"
 # COLOURS #
-black = (0,0,0)
+#colors
 transparent = (0,0,0,0)
 white = (255,255,255)
 grey = (196, 196, 196)
 debugcol = (255, 107, 223)
-linecolor = black #initial line colour
+#themed colors
+lightgreen = (137,191,113)
+mediumgreen = (49,96,76)
+darkgreen = (44,88,89)
+black = (8,17,23)
+brown = (83,106,74)
 
-# SET UP WINDOW #
-(canvasx, canvasy) = (int(Nx*0.6),scrollheight) #define canvas size
-(offsetx, offsety) = (int(Nx*0.2),int(Ny*0.1)) #define borders (derived from canvas size)
-#define the two updatable areas of the screen - drawscreen and trackbar
-ActiveRect = pygame.Rect(offsetx, offsety, Nx - 2*offsetx, Ny - 2*offsety)
-trackerbar = pygame.Rect(0.25*Nx,0.28*offsety,0.5*Nx,0.04*Ny)
+#set up theme
+trackbaroutlinecolor = black
+trackbaremptycolor = white
+trackbarfullcolor = black
+canvascolor = lightgreen
+maskcolor = darkgreen
+linecolor = mediumgreen #initial line color
+endcolor = black #maximum possible line color
 
-
-# SET UP PYGAME #
-pygame.init()
-#make 3 layers
-window = pygame.display.set_mode( (Nx,Ny), pygame.RESIZABLE )
-canvas = pygame.Surface( (canvasx, canvasy) )
-mask = pygame.Surface( (Nx,Ny), pygame.SRCALPHA ) #this is the mask/musicplayer surface
-#color in layers
-window.fill(debugcol)   #decorate window (should never be visible)
-canvas.fill(white)      #blank canvas
-mask.fill(grey)         #border colors
-pygame.draw.rect(mask, transparent,ActiveRect) #cut out hole in mask
-pygame.draw.rect(mask, white, trackerbar) #draw trackerbar
-pygame.draw.rect(mask, black,trackerbar,5) #draw trackerbar outline
-#final touches
-pygame.display.set_caption("Listen And Draw - Now Playing: ",filename)
-clock = pygame.time.Clock() #set up clock object for FPS limiting
-#initial blitting (order is important!)
-window.blit(canvas, (offsetx, offsety))
-window.blit(mask, (0,0))
-pygame.display.update() #update whole window
-
-# SET UP AUDIO PLAYBACK #
-AUDIO = MP3(filename)
-pygame.mixer.music.load(filename) #get ready to stream audio track
-TOTALTIME = math.ceil(AUDIO.info.length) #gets length of track in seconds
-totalframes = TOTALTIME*FPS
-timestep = 1/totalframes #calculate percentage increase per frame (percent goes from 0-1)
-
-# INITIALISE CONSTANTS #
-(prevmousex,prevmousey) = (None, None) #default "previousmouse" location
-revealmode = False
-decimaloffsety = offsety
-timepercent = 0 #number from 0 to 1
-initialoffsety = offsety
-#calculate scrollspeed required
-scrollspeed = ( scrollheight - (Ny - 2*offsety) )/(TOTALTIME*FPS) #in pixels/frame
 
 # FUNCTIONS #
-def scrollcanvas(scrollspeed, decimaloffsety,offsety):
+def borderdraw(pixelsize=5):
+    #generates a random pixellated "bevel" pattern for decorative purposes
+    if canvaswidth % pixelsize == 0 and canvasheight % pixelsize == 0:
+        #only draw pixel bevel if height is multiple of pixelsize to avoid misalignment
+        #DRAW VERTICAL BEVELS
+        for y in range(offsety,offsety+canvasheight,pixelsize):
+            x = offsetx - 2
+            for depth in range(1,6):
+                darkchance = -0.2*depth + 0.084*math.sin(2.6*depth) + 1.05 #equation to generate pixel probabilities
+                if random.random() < darkchance:
+                    mask.fill(black, pygame.Rect(x - depth*pixelsize, y, pixelsize, pixelsize))
+                if random.random() < darkchance:
+                    mask.fill(black, pygame.Rect(x-2 + canvaswidth + depth*pixelsize, y, pixelsize, pixelsize))
+        #DRAW HORIZONTAL BEVELS
+        for x in range(offsetx,offsetx+canvaswidth,pixelsize):
+            y = offsety - 2
+            for depth in range(1,6):
+                darkchance = -0.2*depth + 0.084*math.sin(2.6*depth) + 1.05 #equation to generate pixel probabilities
+                if random.random() < darkchance:
+                    mask.fill(black, pygame.Rect(x, y - depth*pixelsize, pixelsize, pixelsize))
+                if random.random() < darkchance:
+                    mask.fill(black, pygame.Rect(x, y-2 + canvasheight + depth*pixelsize, pixelsize, pixelsize))    #draw trackbar
+    return
+                    
+def initialdraw():
+    pixelsize=5
+    #color in layers
+    window.fill(debugcol)   #fill window with bright pink debug color (should never be visible)
+    canvas.fill(canvascolor)      #blank canvas
+    mask.fill(maskcolor)         #fill mask
+    pygame.draw.rect(mask, transparent,ActiveRect) #cut out hole in mask
+    pygame.draw.rect(mask, black,ActiveRect,pixelsize) #outline mask in dark color
+    #FANCY SCRIPT TO DRAW RANDOM PIXEL BEVELS GOES HERE
+    borderdraw(pixelsize)
+    pygame.draw.rect(mask, trackbaremptycolor, trackbar) #draw trackbar
+    pygame.draw.rect(mask, trackbaroutlinecolor,trackbar,5) #draw trackbar outline
+    #final touches
+    pygame.display.set_caption("Listen And Draw - Now Playing: ",filename)
+    window.blit(canvas, (offsetx, offsety))
+    window.blit(mask, (0,0))
+    pygame.display.update() #update whole window
+    return
+
+def windowmeasurements(Nx,Ny):
+    """
+    This script takes in the window size and centers/arrange the canvas, Rect 
+    objects and trackbar, spitting out a whole slew of constants
+    """
+    (canvasx, canvasy) = (scrollwidth, int(Nx*0.8))
+    (canvaswidth, canvasheight) = (int(0.8*Nx),int(0.6*Ny))        
+    (offsetx, offsety) = ( int( (Nx-canvaswidth) /2 ), int( (Ny-canvasheight)/2 )) #define borders (derived from canvas size)
+    (trackbarwidth,trackbarheight) = (0.5*Nx, 0.04*Ny)
+    #define the two updatable areas of the screen - drawscreen and trackbar
+    ActiveRect = pygame.Rect(offsetx, offsety,canvaswidth, canvasheight)
+    if trackbarposition == "under":
+        trackbar = pygame.Rect(int((Nx-trackbarwidth)/2), Ny - trackbarheight - int((offsety-trackbarheight)/2),trackbarwidth,trackbarheight)
+    else:
+        trackbar = pygame.Rect(int((Nx-trackbarwidth)/2), int((offsety-trackbarheight)/2),trackbarwidth,trackbarheight)
+    return(canvasx,canvasy,canvaswidth, canvasheight, offsetx, offsety,trackbarwidth,trackbarheight,ActiveRect,trackbar)
+
+def scrollcanvas(scrollspeed, decimaloffsetx,offsetx):
     """
     This function scrolls the canvas. Its designed to reach the bottom just as
     the song ends, plus or minus a frame.
     """
-    decimaloffsety -= scrollspeed
-    offsety = round(decimaloffsety) #offset is in pixels, so has to be an integer
-    return decimaloffsety,offsety
+    decimaloffsetx -= scrollspeed
+    offsetx = round(decimaloffsetx) #offset is in pixels, so has to be an integer
+    return decimaloffsetx,offsetx
 
 def changeline(timepercent,linecolor,thickness):
     """
     This function may be changed depending on artistic effect desired.
-    CURRENTLY:
-        - Thickness increases linearly from 1 - 5
-        - Line color changes linearly from (0,0,0) to (255,0,0)
+    Currently it does nothing.
     """
-    thickness = round(1 + 4*timepercent)
-    linecolor = (int(timepercent*255),linecolor[1],linecolor[2])
-    print(linecolor)
+    #thickness = round(3 + math.sin(timepercent*4*math.pi))
+    #linecolor = (int(timepercent*255),linecolor[1],linecolor[2])
     return linecolor,thickness
 
-def drawsmoothline(mousex,mousey,prevmousex,prevmousey,color,thickness):
+def drawsmoothline(mousex,mousey,prevmousex,prevmousey,color,endcolor,thickness):
     """
     This smoothline is split into 3 drawing methods
     1. For thickness from 0 - 1.99, it draws a single aliased pixel line
     2. For thickness from 2 - 3.99 it draws a single line surrounded by 4 aliased lines and ended with a circle
     3. For thickness from 4 + it draws a fat single line bookended by circles at both ends
     """
+    
     if prevmousex == None or prevmousey == None: #if its the first frame of a linedraw
         (prevmousex,prevmousey) = (mousex,mousey)
         if int(thickness) > 1:
             pygame.draw.circle(canvas, color, (mousex,mousey), int(thickness))
+    
+    #CHANGE COLOUR BASED ON SPEED
+    startcolor = color
+    endcolor = endcolor
+    Rcolorchange = -startcolor[0] + endcolor[0]
+    Gcolorchange = -startcolor[1] + endcolor[1]
+    Bcolorchange = -startcolor[2] + endcolor[2]
+    
+    distance = math.sqrt( (mousex-prevmousex)**2 + (mousey-prevmousey)**2   )
+    colorpercent = min(1,distance/60)
+    RED = startcolor[0] + Rcolorchange*colorpercent
+    GREEN = startcolor[1] + Gcolorchange*colorpercent
+    BLUE = startcolor[2] + Bcolorchange*colorpercent 
+   
+    color = (int(RED),int(GREEN),int(BLUE))
     #idea: draw multiple smooth lines to simulate thickness
     if int(thickness) == 1:
         pygame.draw.aaline(canvas, color, (prevmousex,prevmousey), (mousex,mousey))
@@ -121,12 +175,53 @@ def drawsmoothline(mousex,mousey,prevmousex,prevmousey,color,thickness):
         pygame.draw.circle(canvas, color, (prevmousex,prevmousey), int(thickness)-1)
     return
 
-def drawtrackbar(timepercent):
-    pygame.draw.rect(mask, black,pygame.Rect(0.25*Nx,0.28*offsety,timepercent*0.5*Nx,0.04*Ny) )
+def updatetrackbar(timepercent):
+    if trackbarposition == "under":
+       pygame.draw.rect(mask, black,pygame.Rect(int((Nx-trackbarwidth)/2),int(Ny - trackbarheight-(offsety-trackbarheight)/2),timepercent*trackbarwidth,trackbarheight) ) 
+    else: #otherwise display on top
+        pygame.draw.rect(mask, black,pygame.Rect(int((Nx-trackbarwidth)/2),int((offsety-trackbarheight)/2),timepercent*trackbarwidth,trackbarheight) )
 
-pygame.mixer.music.play()
+"""
+##################################################################################################
+    MAIN PROGRAM START
+################################################################################################## 
+"""
+# SET UP WINDOW USING windowmeasurements FUNCTION#
+(canvasx,canvasy,canvaswidth, canvasheight, offsetx, offsety,trackbarwidth,trackbarheight,ActiveRect,trackbar)\
+= windowmeasurements(Nx,Ny)
+
+# INITIALISE PYGAME #
+pygame.init()
+
+# DRAW UI #
+#make 3 layers
+window = pygame.display.set_mode( (Nx,Ny), pygame.RESIZABLE )
+canvas = pygame.Surface( (canvasx, canvasy) )
+mask = pygame.Surface( (Nx,Ny), pygame.SRCALPHA ) #this is the mask/musicplayer surface
+#INITIALDRAW FUNCTION
+initialdraw()
+clock = pygame.time.Clock() #set up clock object for FPS limiting
+
+# SET UP AUDIO PLAYBACK #
+AUDIO = MP3(filename)
+pygame.mixer.music.load(filename) #get ready to stream audio track
+TOTALTIME = math.ceil(AUDIO.info.length)*loopnumber #gets length of track in seconds
+totalframes = TOTALTIME*FPS
+timestep = 1/totalframes #calculate percentage increase per frame (percent goes from 0-1)
+
+# INITIALISE CONSTANTS #
+(prevmousex,prevmousey) = (None, None) #default "previousmouse" location
+revealmode = False
+decimaloffsetx = offsetx
+timepercent = 0 #number from 0 to 1
+initialoffsetx = offsetx
+#calculate scrollspeed required
+scrollspeed = ( scrollwidth - (Nx - 2*offsetx) )/(TOTALTIME*FPS) #in pixels/frame
+
+pygame.mixer.music.play(loopnumber)
 while True:
     clock.tick(FPS)
+    #print(int(clock.get_fps()))
     timepercent += timestep
     for event in pygame.event.get(): #detect events
         if event.type == pygame.QUIT: #detect attempted exit
@@ -135,34 +230,46 @@ while True:
         if pygame.mouse.get_pressed()[0]:
             mousex = int(pygame.mouse.get_pos()[0]) - offsetx
             mousey = int(pygame.mouse.get_pos()[1]) - offsety
-            drawsmoothline(mousex,mousey, prevmousex,prevmousey,linecolor,thickness)
+            drawsmoothline(mousex,mousey, prevmousex,prevmousey,linecolor,endcolor,thickness)
             #drawline(mousex,mousey, prevmousex,prevmousey,linecolor,thickness)
             (prevmousex,prevmousey) = (mousex,mousey) #update prev values               
         if not pygame.mouse.get_pressed()[0]:
             (prevmousex,prevmousey) = (None,None)
-        if pygame.mouse.get_pressed()[2]: #DEBUG REVEAL FUNCTIONALITY
-            revealmode = True
+        if (pygame.mouse.get_pressed()[0] and revealmode == True) or pygame.mouse.get_pressed()[2]: #DEBUG RESTART FUNCTIONALITY
+            (canvasx,canvasy,canvaswidth, canvasheight, offsetx, offsety,trackbarwidth,trackbarheight,ActiveRect,trackbar)\
+= windowmeasurements(Nx,Ny)
+            initialdraw()
+            (prevmousex,prevmousey) = (None, None) #default "previousmouse" location
+            revealmode = False
+            decimaloffsetx = offsetx
+            timepercent = 0 #number from 0 to 1
+            initialoffsetx = offsetx
+            #calculate scrollspeed required
+            scrollspeed = ( scrollwidth - (Nx - 2*offsetx) )/(TOTALTIME*FPS) #in pixels/frame
+            
+            pygame.mixer.music.play(loopnumber)
             
     if revealmode:
         pygame.mixer.music.stop()
         offsety = 0
-        Nx,Ny = Nx,scrollheight
-        window = pygame.display.set_mode((Nx, scrollheight), pygame.RESIZABLE)
+        Nx,Ny = scrollwidth,Ny
+        window = pygame.display.set_mode((Nx, Ny), pygame.RESIZABLE)
         mask.fill(transparent)
         window.fill(grey)
-        window.blit(canvas,(offsetx,0)) #NB offsety is now 0
-        window.blit(mask,(offsetx,0))
+        window.blit(canvas,(0,offsety)) #NB offsety is now 0
+        window.blit(mask,(0,offsety))
         pygame.display.update()
         pygame.image.save(canvas,"video/testcanvas.png")
-    elif not revealmode and offsety < -340:
+    elif not revealmode and timepercent >= 1:
         revealmode = True
     else:
-        decimaloffsety, offsety = scrollcanvas(scrollspeed,decimaloffsety,offsety)
+        updatetrackbar(timepercent)
+        decimaloffsetx, offsetx = scrollcanvas(scrollspeed,decimaloffsetx,offsetx)
         linecolor,thickness = changeline(timepercent,linecolor,thickness)
         window.blit(canvas, (offsetx, offsety) )
         window.blit(mask, (0,0) )
         pygame.display.update(ActiveRect)
-        pygame.display.update(trackerbar)
+        pygame.display.update(trackbar)
     
     #add an if-statement to shunt program into revealmode when ticker reaches bottom
     
